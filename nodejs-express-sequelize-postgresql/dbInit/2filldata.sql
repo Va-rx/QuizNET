290a217de18324245c2bdd7251a8b7ec6492c6a0
Jakub Płowiec <frytkaspl@gmail.com>
2026-03-03 23:28:34 +0100

    new password admin

--- nodejs-express-sequelize-postgresql/dbInit/2filldata.sql
+++ nodejs-express-sequelize-postgresql/dbInit/2filldata.sql
@@ -1,4 +1,4 @@
 INSERT INTO roles (description) VALUES ('Student'), ('Admin');
 
-INSERT into users (email, index, name, surname, nickname, password, role_id) VALUES ('admin@admin.pl', null, 'admin', 'admin', 'admin', '$2b$10$pMcH4XkCzVPLTbpP/ng6IOURR5lE/TzP8LH0PGry4s0NFIIFOSuZS', 2);  --admin123ADMIN
+INSERT into users (email, index, name, surname, nickname, password, role_id) VALUES ('admin@admin.pl', null, 'admin', 'admin', 'admin', '$2b$10$R4UYPwELz5B4xmEq7L/DL.pjib6eALumwEsMCPeaIxamIEABzF2cu', 2);  --adminADMIN
 INSERT INTO games (name, description) VALUES ('Czołgi', 'Strzelaj i broń się przed wrogami'), ('Deathmatch', 'Walcz z pozostałymi graczmi o pytania'), ('Platformer', 'Skacz i uważaj na przeszkody');
