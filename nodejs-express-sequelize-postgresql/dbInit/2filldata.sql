2957069c4b09e8cce539449e189c7becd393bb51
Jakub Płowiec <frytkaspl@gmail.com>
2026-03-03 18:28:30 +0100

    deleted redundant user init at docker composer

--- nodejs-express-sequelize-postgresql/dbInit/2filldata.sql
+++ nodejs-express-sequelize-postgresql/dbInit/2filldata.sql
@@ -1,7 +1,4 @@
 INSERT INTO roles (description) VALUES ('Student'), ('Admin');
 
-INSERT into users (email, index, name, surname, nickname, password, role_id) VALUES ('', null, 'admin', 'admin', 'admin', '$2b$10$7f7LnJzhWSo8iFovJyRoje73CL6B6YeU8sOWYR8kwe0F9ir.ECH.a', 2),  --admin123
-                                                                           ('test@student.agh.edu.pl', 213700, 'test', 'test', 'test', '$2b$10$GD7pKKuLbq6K8.QL1U95G.tMOnMsNwI.7KHzBpsqXYH.WMPJzmCzC', 1);   --test123
-
-
+INSERT into users (email, index, name, surname, nickname, password, role_id) VALUES ('admin@admin.pl', null, 'admin', 'admin', 'admin', '$2b$10$pMcH4XkCzVPLTbpP/ng6IOURR5lE/TzP8LH0PGry4s0NFIIFOSuZS', 2);  --admin123ADMIN
 INSERT INTO games (name, description) VALUES ('Czołgi', 'Strzelaj i broń się przed wrogami'), ('Deathmatch', 'Walcz z pozostałymi graczmi o pytania'), ('Platformer', 'Skacz i uważaj na przeszkody');
